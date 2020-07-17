3b4ee49734761794570a1547c561f33cc4629477
Diego Lastorta <diego.lastorta@snagajob.com>
2020-07-17 12:05:13 -0300

    Added Express

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-const http = require('http');
-const server = http.createServer((req,res) =>{
-    if(req.url === '/'){
-        res.write('Hello World');
-        res.end();
-    }
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
-server.listen(3000);
+app.listen(3000, () => console.log("Listening on port 3000...."));
 
-console.log('Listening on port 3000....');
 /*
 var meetup = require('meetup-api')({
 	oauth: JSON.parse(process.env.MEETUP_OAUTH) //env variable with oauthtoken
