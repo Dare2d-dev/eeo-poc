84b12f63713de948498300ae432cc45e8144f1a5
Diego Lastorta <diego.lastorta@snagajob.com>
2020-07-17 10:00:54 -0300

    Hello World

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+const http = require('http');
+const server = http.createServer((req,res) =>{
+    if(req.url === '/'){
+        res.write('Hello World');
+        res.end();
+    }
+});
+
+server.listen(3000);
+
+console.log('Listening on port 3000....');
+/*
 var meetup = require('meetup-api')({
 	oauth: JSON.parse(process.env.MEETUP_OAUTH) //env variable with oauthtoken
 });
@@ -11,4 +23,4 @@ meetup.getOAuthRequestToken((error, Url) => {
 
 meetup.getOAuthAccessToken(oauth_token, () => {
 	//API commands goes here
-});
+});*/
